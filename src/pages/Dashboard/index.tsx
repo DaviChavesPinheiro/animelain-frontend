ee7f090abc3a0800ae4135808a3ed98711a53242
Davi <davicpinheiro123@hotmail.com>
2020-12-25 22:59:07 -0300

    Dashboard: Create New Anime Component Link Created

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { FiAlertCircle, FiPlayCircle } from 'react-icons/fi';
+import { FiAlertCircle, FiPlayCircle, FiPlus } from 'react-icons/fi';
 import { Anime, Container, Content, ImageContainer } from './styles';
 import api from '../../services/api';
 import Header from '../../components/Header';
@@ -26,6 +26,18 @@ const Dashboard: React.FC = () => {
       <Header />
 
       <Content>
+        <Anime to="/animes/create">
+          <ImageContainer>
+            <FiPlus size="30" color="#565656" />
+          </ImageContainer>
+          <div>
+            <h2>Criar Novo</h2>
+            <p>Clique para criar um novo anime</p>
+            <div>
+              <span />
+            </div>
+          </div>
+        </Anime>
         {animes.map(anime => (
           <Anime key={anime.id} to={`/animes/${anime.id}`}>
             <ImageContainer>
